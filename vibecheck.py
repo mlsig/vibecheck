ed3f079b45a3e5dc0f67026e8b3a6ec460bcd652
Marisa Sigas <mlsig95@gmail.com>
2020-09-30 20:43:01 -0400

    remove client credentials
    note to self - will implement something like dotenv for the future

--- vibecheck.py
+++ vibecheck.py
@@ -12,13 +12,13 @@ def avg_valnc(tracks,psize):
         valnc = valnc + float(feats[0]['energy'])
     return valnc / psize
         
-credentials = oauth2.SpotifyClientCredentials(client_id='26b486e6ddb841d190834b7dce27c20a', client_secret='f57c30c5d2e44d8385a384b85c04a687')
+credentials = oauth2.SpotifyClientCredentials(client_id='', client_secret='')
 token = credentials.get_access_token()
 token = util.prompt_for_user_token(
         username='gleekyninja22',
         scope='user-top-read',
-        client_id='26b486e6ddb841d190834b7dce27c20a',
-        client_secret='f57c30c5d2e44d8385a384b85c04a687',
+        client_id='',
+        client_secret='',
         redirect_uri='http://google.com')
 spotify = spotipy.Spotify(auth=token)
 
